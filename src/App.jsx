329cc1d82ef0c88efea2f9d581f47d8c2223005c
Miguel Salazar <yobebe@hotmail.com.ar>
2024-03-31 22:13:32 -0300

    Creando el repo para un nuevo proyecto de react.js, en este caso sera una app del clima

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// Importaciones 
+import './App.css'
+
+const App = () => {
+  // Logica de la aplicacion
+  
+  return (
+    <>
+      <h1 className='text-center' > App weather </h1>
+    </>
+  )
+}
+
+export default App
